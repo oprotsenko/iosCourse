3e7a0e0dbf34d61f076198e2553df99e12c2390a
olena.protsenko <ext-olena.protsenko@here.com>
2023-12-17 00:02:20 +0200

    Initial Commit

---
+++ iosCourse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosCourse
+//
+//  Created by olena.protsenko on 17.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
